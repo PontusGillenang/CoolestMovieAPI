086cf5c769e9c0b8aa5faf5850d20c89e2e2aff9
PontusGillenang <55433424+PontusGillenang@users.noreply.github.com>
2020-05-05 14:40:43 +0200

    Added DirectorController and DirectorDTO
    Created DirectorController and DirectorDTO classes and made preliminary Get requests and properties.

---
+++ CoolestMovieAPI/CoolestMovieAPI/Controllers/DirectorController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoolestMovieAPI.Controllers
+{
+    [Route("api/v1.0/[controller]")]
+    [ApiController]
+    public class DirectorController : ControllerBase
+    {
+        private readonly DirectorRepository _repository;
+
+        [HttpGet]
+        public Task<Director> GetAll()
+        {
+            return _repository.GetAllDirector();
+        }
+
+        [HttpGet("{id}")]
+        public Task<Director> GetById(int id)
+        {
+            return _repository.GetDirectorById(id);
+        }
+
+        [HttpGet("name={name}")]
+        public Task<Director> GetByName(string name)
+        {
+            return _repository.GetDirectorByName(name);
+        }
+
+        [HttpGet("country={country}")]
+        public Task<Director> GetByCountry(string country)
+        {
+            return _repository.GetDirectorByCountry(country);
+        }
+    }
+}
---
+++ CoolestMovieAPI/CoolestMovieAPI/DTO/DirectorDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoolestMovieAPI.DTO
+{
+    public class DirectorDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Country { get; set; }
+    }
+}
