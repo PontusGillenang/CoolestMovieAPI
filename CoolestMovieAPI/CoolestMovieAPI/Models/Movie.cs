ce0781f4552268a79ef93e54658f91d0b64682ef
simbasang <sebastian.englund@live.se>
2020-05-06 15:51:41 +0200

    name

--- CoolestMovieAPI/CoolestMovieAPI/Models/Movie.cs
+++ CoolestMovieAPI/CoolestMovieAPI/Models/Movie.cs
@@ -8,7 +8,7 @@ namespace CoolestMovieAPI.Models
     public class Movie
     {
         public int ID { get; set; }
-        public string Name { get; set; }
+        public string Title { get; set; }
         public List<MovieDirector> movieDirectors { get; set; }
         public List<MovieActor> MovieActors { get; set; }
         public List<Trailer> Trailers { get; set; }
