d0a830aa2390618a616a6b90d964fd2b2d7c17aa
PontusGillenang <55433424+PontusGillenang@users.noreply.github.com>
2020-05-14 16:51:09 +0200

    Tested GetAll

--- CoolestMovieAPI/CoolestMovieAPI.Tests/DirectorsControllerTest.cs
+++ CoolestMovieAPI/CoolestMovieAPI.Tests/DirectorsControllerTest.cs
@@ -1,14 +1,33 @@
-﻿using CoolestMovieAPI.Models;
+﻿using CoolestMovieAPI.Controllers;
+using CoolestMovieAPI.Models;
 using CoolestMovieAPI.MovieDbContext;
+using CoolestMovieAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace CoolestMovieAPI.Tests
 {
     public class DirectorsControllerTest
     {
-        
+        [Fact]
+        public async Task GetAll_ReturnsOK()
+        {
+            // Arange
+            var directorRepositoryMock = new Mock<IDirectorRepository>();
+            directorRepositoryMock.Setup(repo => repo.GetAllDirectors()).ReturnsAsync((IList<Director>)null);
+            var controller = new DirectorsController(directorRepositoryMock.Object);
+
+            // Act
+            var result = await controller.GetAll();
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result.Result);
+        }
     }
+
 }
--- CoolestMovieAPI/CoolestMovieAPI/Controllers/DirectorsController.cs
+++ CoolestMovieAPI/CoolestMovieAPI/Controllers/DirectorsController.cs
@@ -1,6 +1,7 @@
 ﻿using CoolestMovieAPI.Models;
 using CoolestMovieAPI.MovieDbContext;
 using CoolestMovieAPI.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using System;
@@ -15,22 +16,30 @@ namespace CoolestMovieAPI.Controllers
     public class DirectorsController : ControllerBase
     {
         private readonly IDirectorRepository _directorRepository;
-        
+
         public DirectorsController(IDirectorRepository directorRepository)
         {
             _directorRepository = directorRepository;
         }
 
         [HttpGet]
-        public Task<IList<Director>> GetAll()
+        public async Task<ActionResult<IList<Director>>> GetAll()
         {
-            return _directorRepository.GetAllDirectors();
+            try
+            {
+                var result = await _directorRepository.GetAllDirectors();
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status404NotFound, $"Could not find object: {e.Message}");
+            }
         }
 
         [HttpGet("{id}")]
-        public Task<Director> GetById(int id)
+        public  Task<Director> GetById(int id)
         {
-            return _directorRepository.GetDirectorById(id);
+                return _directorRepository.GetDirectorById(id);
         }
 
         [HttpGet("name={name}")]
