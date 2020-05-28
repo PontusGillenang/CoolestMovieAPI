df4ea549da2339db4030e13033d220d1cbc11218
Zorkii <56868345+aohzork@users.noreply.github.com>
2020-05-28 22:45:48 +0200

    MOV-207 Create GenreRepository

--- CoolestMovieAPI/CoolestMovieAPI/Services/GenreRepository.cs
+++ CoolestMovieAPI/CoolestMovieAPI/Services/GenreRepository.cs
@@ -16,7 +16,7 @@ namespace CoolestMovieAPI.Services
         {
         }
 
-        public async Task<IList<Genre>> GetAllGenres()
+        public async Task<IList<Genre>> GetAllGenre()
         {
             var query = _movieContext.Genre;
 
@@ -37,15 +37,6 @@ namespace CoolestMovieAPI.Services
             _logger.LogInformation($"Getting genre by name: {name}.");
 
             return await _movieContext.Genre.Where(g => g.GenreType.Contains(name)).ToListAsync();
-        }
-
-
-        //for cleanup. technical problems with github desktop.
-        //have to have this method until merge of MOV-206, where this method is removed.
-        public async Task<IList<Genre>> GetMoviesByGenre(string name)
-        {
-
-            return await ;
         }
     }
 }
